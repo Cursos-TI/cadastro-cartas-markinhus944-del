bcc6accc073b3d99fcd40b2e7f4788f4de7af073
markinhus944-del <markinhus944@gmail.com>
2026-03-11 22:35:05 +0000

    Coloquei Estado nome e codigo de carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,21 @@
-#include <stdio.h>
+#include <stdio.h>{
+  #include <string.h> // para usar strcspn
+    int main() {
+    int Populacao,Numero_de_pontos_turisticos;
+    char Estado[50],Nome_da_cidade[50],Codigo_da_carta[50];
+    float Area_em_km2,PIB;
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+    printf("Digite o nome da Cidade:\n");
+    fgets(Nome_da_cidade,sizeof(Nome_da_cidade),stdin);
+    Nome_da_cidade[strcspn(Nome_da_cidade, "\n")] = '\0'; // remove o ENTER do fgets
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+  printf("Digite o nome do estado:\n");
+  fgets(Estado,sizeof(Estado),stdin);
+  Estado[strcspn(Estado, "\n")] = '\0'; // remove o ENTER do fgets
 
-  // Área para entrada de dados
-
-  // Área para exibição dos dados da cidade
-
-return 0;
-} 
+  printf("Digite O Codigo da Carta:)\n");
+  scanf("%d",&Codigo_da_carta);
+  
+    
+   return 0;
+}
