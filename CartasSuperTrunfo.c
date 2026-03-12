9c4241d2ac14fd8c30c9d0f61e3fc223e1788de4
markinhus944-del <markinhus944@gmail.com>
2026-03-12 00:21:50 +0000

    Primeiro cadastro funcionando

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,4 @@
-#include <stdio.h>{
+#include <stdio.h>
   #include <string.h> // para usar strcspn
     int main() {
     int Populacao,Numero_de_pontos_turisticos;
@@ -13,9 +13,25 @@
   fgets(Estado,sizeof(Estado),stdin);
   Estado[strcspn(Estado, "\n")] = '\0'; // remove o ENTER do fgets
 
-  printf("Digite O Codigo da Carta:)\n");
-  scanf("%d",&Codigo_da_carta);
+  printf("Digite o Codigo da Carta:)\n");
+  scanf("%s", Codigo_da_carta);
   
-    
+    printf("Digite o Numero Total da Populacao:\n");
+    scanf("%d",&Populacao);
+
+    printf("Digite o Numero total de pontos turisticos:\n");
+scanf("%d",&Numero_de_pontos_turisticos);
+
+printf("Digite a Area em km2:\n");
+scanf("%f",&Area_em_km2);
+
+printf("Digite O PIB da cidade:\n");
+scanf("%f",&PIB);
+
+printf("\nNome da Cidade:%s Estado: %s",Nome_da_cidade,Estado  );
+printf("\nCodigo da Carta:%s\nPopulacao:%d \nN Pontos Turistico:%d",Codigo_da_carta,Populacao,Numero_de_pontos_turisticos);
+printf("\nArea-Km2:%.2f km2",Area_em_km2);
+printf("\nPIB:%.2f Bilhoes",PIB);
+
    return 0;
 }
