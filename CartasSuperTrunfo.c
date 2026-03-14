685e51402dd16b277ef9740f4075e55b52a6dbe8
markinhus944-del <markinhus944@gmail.com>
2026-03-14 03:04:27 +0000

    coloquei cauculo de Densidade populacional , pib per capito  e corrigi um erro onde a populacao da carta 1 aparecia na 2.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,7 +9,8 @@ char Estado2[50],Nome_da_cidade2[50],Codigo_da_carta2[50];
 
 float Area_em_km2,PIB;
 float Area_em_km22,PIB2;
-
+float densidade_populacional,pib_percapita;
+float densidade_populacional2,pib_percapita2;
     printf("(Carta 1) Digite o nome da Cidade:\n");
     fgets(Nome_da_cidade,sizeof(Nome_da_cidade),stdin);
     Nome_da_cidade[strcspn(Nome_da_cidade, "\n")] = '\0'; // remove o ENTER do fgets
@@ -58,6 +59,15 @@ scanf("%f",&Area_em_km22);
 printf("Digite O PIB da cidade:\n");
 scanf("%f",&PIB2);
 
+pib_percapita = (PIB * 1000000000) / Populacao;
+densidade_populacional = Populacao / Area_em_km2;
+
+pib_percapita2 = (PIB2 * 1000000000) / Populacao2 ;
+densidade_populacional2 = Populacao2 / Area_em_km22 ;
+
+
+
+
 
 
 
@@ -69,12 +79,17 @@ printf("\nNome da Cidade:%s Estado: %s",Nome_da_cidade,Estado  );
 printf("\nCodigo da Carta:%s\nPopulacao:%d \nN Pontos Turistico:%d",Codigo_da_carta,Populacao,Numero_de_pontos_turisticos);
 printf("\nArea-Km2:%.2f km2",Area_em_km2);
 printf("\nPIB:%.2f Bilhoes",PIB);
+printf("\nDensidade Populacional:%.2f",densidade_populacional);
+printf("\nPib Per_Capito:%.2f",pib_percapita);
+
 // MOstra carta 2
 printf("\n---Carta 2: %s ---", Nome_da_cidade2);
 printf("\nNome da Cidade:%s Estado: %s",Nome_da_cidade2,Estado2  );
-printf("\nCodigo da Carta:%s\nPopulacao:%d \nN Pontos Turistico:%d",Codigo_da_carta2,Populacao,Numero_de_pontos_turisticos2);
+printf("\nCodigo da Carta:%s\nPopulacao:%d \nN Pontos Turistico:%d",Codigo_da_carta2,Populacao2,Numero_de_pontos_turisticos2);
 printf("\nArea-Km2:%.2f km2",Area_em_km22);
 printf("\nPIB:%.2f Bilhoes",PIB2);
+printf("\nDensidade Populacional:%.2f",densidade_populacional2);
+printf("\nPib Per_Capito:%.2f",pib_percapita2);
 
 
 
